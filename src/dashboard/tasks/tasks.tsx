dcd22a04f0c865c39620a5a15a36e09ec72207ed
Fatur Muhammad <fatur_muhammad@bk.ru>
2023-12-16 00:07:11 +0700

    remove data log

--- src/dashboard/tasks/tasks.tsx
+++ src/dashboard/tasks/tasks.tsx
@@ -25,7 +25,6 @@ export default function Task()
 
         tasks.findAll().then((data) =>{
             setProduct(data);
-            console.log(data);
             
             setLoading(false);
         }).catch((error) => {
