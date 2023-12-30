c931a0d655d26e3ec47965b538f7fb7421e13c7c
Fatur Muhammad <fatur_muhammad@bk.ru>
2023-12-30 20:30:35 +0700

    add gh pages support route

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,19 @@ function App() {
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={<Login/>}/>
-                    <Route path='/home' element={<Home/>}/>
-                    <Route path='/products' element={< Product />}/>
-                    <Route path='/activity' element={< WorkerTask />}/>
-                    <Route path='/users' element={< User />}/>
-                    {/* <Route path='team' element={< Team title="Team" />}/>
-					<Route path='invoices' element={< Invoices title="Invoices" />}/>
-					<Route path='calendar' element={< Calendar title="Calendar" />}/>
-					<Route path='faq' element={< FAQ title="FAQ" />}/>
-					<Route path='form' element={< Form title="Form" />}/>
-					<Route path='bar' element={< Bar title="Bar" />}/> */}
+                    <Route path="/The-Hub">
+                        <Route index element={<Login/>}/>
+                        <Route path='home' element={<Home/>}/>
+                        <Route path='products' element={< Product />}/>
+                        <Route path='activity' element={< WorkerTask />}/>
+                        <Route path='users' element={< User />}/>
+                        {/* <Route path='team' element={< Team title="Team" />}/>
+                        <Route path='invoices' element={< Invoices title="Invoices" />}/>
+                        <Route path='calendar' element={< Calendar title="Calendar" />}/>
+                        <Route path='faq' element={< FAQ title="FAQ" />}/>
+                        <Route path='form' element={< Form title="Form" />}/>
+                        <Route path='bar' element={< Bar title="Bar" />}/> */}
+                    </Route>
                 </Routes>
                 <Toaster position="top-right" reverseOrder={false}/>
         	</BrowserRouter>
