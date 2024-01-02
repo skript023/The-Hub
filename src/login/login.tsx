a09ab06e5fbd6b1bc1feb3defb4afd4d3136560c
Fatur Muhammad <fatur_muhammad@bk.ru>
2024-01-02 14:02:36 +0700

    change logo in login page

--- src/login/login.tsx
+++ src/login/login.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -9,7 +8,6 @@ import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import authentication from '../api/authentication';
@@ -83,10 +81,8 @@ export default function Login()
                             alignItems: 'center',
                         }}
                     >
-                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                            <LockOutlinedIcon />
-                        </Avatar>
-                        <Typography component="h1" variant="h5">
+                        <img src='/logo-icon.png'/>
+                        <Typography component="h1" variant="h5" sx={{ marginTop: 1 }}>
 							Sign in
                         </Typography>
                         <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
