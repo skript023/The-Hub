0460f367cbaccadd9d558336acd74129099c41ce
Fatur Muhammad <fatur_muhammad@bk.ru>
2024-01-02 21:54:29 +0700

    change dialog transition

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -3,6 +3,9 @@ import DialogTitle from '@mui/material/DialogTitle';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import { styled } from '@mui/material/styles';
+import { forwardRef } from 'react';
+import { TransitionProps } from '@mui/material/transitions';
+import { Slide } from '@mui/material';
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
     '& .MuiDialogContent-root': {
@@ -11,7 +14,17 @@ const BootstrapDialog = styled(Dialog)(({ theme }) => ({
     '& .MuiDialogActions-root': {
       padding: theme.spacing(1),
     },
-  }));
+}));
+
+  const Transition = forwardRef(function Transition(
+    props: TransitionProps & {
+      children: React.ReactElement<any, any>;
+    },
+    ref: React.Ref<unknown>,
+    ) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
 export default function Modals({open, callback, children, title} : any)
 {
     return (
@@ -19,6 +32,7 @@ export default function Modals({open, callback, children, title} : any)
             onClose={callback}
             aria-labelledby="customized-dialog-title"
             open={open}
+            TransitionComponent={Transition}
         >
             <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
                 {title}
