40f63781c82b65c353b409b5f0f69be6ba3f0da2
Fatur Muhammad <fatur_muhammad@bk.ru>
2024-02-19 15:27:11 +0700

    change callback

--- src/dashboard/tasks/add.tsx
+++ src/dashboard/tasks/add.tsx
@@ -8,7 +8,7 @@ import authentication from "../../api/authentication";
 import dayjs, { Dayjs } from "dayjs";
 import { toast } from "../../components/snackbar";
 
-export default function AddTask({callback}: any)
+export default function AddTask({activities}: any)
 {
     const [startDate, setStartDate] = useState<Dayjs>(dayjs(new Date()))
     const [endDate, setEndDate] = useState<Dayjs>(dayjs(new Date()))
@@ -51,7 +51,7 @@ export default function AddTask({callback}: any)
                 setAlert(() => (<Alert severity="error">{response?.message}</Alert>))
                 toast.error("Add Task", `${response?.message}`);
             }
-            callback(formData);
+            activities.push(formData);
             setLoading(false);
         }).catch((error: any) => {
             toast.error("Add Task", error.message);
