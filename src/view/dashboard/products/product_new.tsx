38fca46886ac03475323a898c93c676677f166ac
Fatur Muhammad <fatur_muhammad@bk.ru>
2024-07-14 00:01:56 +0700

    fix UI issue too small

--- src/view/dashboard/products/product_new.tsx
+++ src/view/dashboard/products/product_new.tsx
@@ -14,58 +14,58 @@ import useTheme from '@mui/material/styles/useTheme';
 import Sidenav from '@/view/navigation/sidebar';
 
 interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
+	children?: React.ReactNode;
+	index: number;
+	value: number;
 }
 
 function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
+	const { children, value, index, ...other } = props;
 
-  return (
-	<div
-	  role="tabpanel"
-	  hidden={value !== index}
-	  id={`tabpanel-${index}`}
-	  aria-labelledby={`tab-${index}`}
-	  {...other}
-	>
-	  {value === index && (
-		<Box p={3}>
-		  {children}
-		</Box>
-	  )}
-	</div>
-  );
+	return (
+		<div
+		role="tabpanel"
+		hidden={value !== index}
+		id={`tabpanel-${index}`}
+		aria-labelledby={`tab-${index}`}
+		{...other}
+		>
+		{value === index && (
+			<Box p={3} sx={{ width: '100%', height: '100%' }}>
+			{children}
+			</Box>
+		)}
+		</div>
+	);
 }
 
 const columns = [
-  { name: 'product', label: 'Product' },
-  { name: 'unit', label: 'Unit of Measure' },
-  { name: 'productLine', label: 'Product Line' },
-  { name: 'productClass', label: 'Product Class' },
-  { name: 'structureType', label: 'Structure Type' },
-  { name: 'lockedBy', label: 'Locked By' },
-  { name: 'eligibility', label: 'Eligibility', options: { customBodyRender: (value: boolean) => (value ? 'Yes' : 'No') } },
+	{ name: 'product', label: 'Product' },
+	{ name: 'unit', label: 'Unit of Measure' },
+	{ name: 'productLine', label: 'Product Line' },
+	{ name: 'productClass', label: 'Product Class' },
+	{ name: 'structureType', label: 'Structure Type' },
+	{ name: 'lockedBy', label: 'Locked By' },
+	{ name: 'eligibility', label: 'Eligibility', options: { customBodyRender: (value: boolean) => (value ? 'Yes' : 'No') } },
 ];
 
 const data = [
-  { product: 'Origin Dependent Routing Non-Recurring Charge', unit: 'Per Month', productLine: 'Origin Dependent Routing PS', productClass: 'Customizable', structureType: '', lockedBy: '', eligibility: false },
-  { product: 'Call Distribution', unit: 'Month', productLine: 'Call Distribution PS', productClass: 'Customizable', structureType: '', lockedBy: '', eligibility: false },
-  // Add more rows here
+	{ product: 'Origin Dependent Routing Non-Recurring Charge', unit: 'Per Month', productLine: 'Origin Dependent Routing PS', productClass: 'Customizable', structureType: '', lockedBy: '', eligibility: false },
+	{ product: 'Call Distribution', unit: 'Month', productLine: 'Call Distribution PS', productClass: 'Customizable', structureType: '', lockedBy: '', eligibility: false },
+	// Add more rows here
 ];
 
 const versionColumns = [
-  { name: 'version', label: 'Version' },
-  { name: 'startDate', label: 'Start Date' },
-  { name: 'endDate', label: 'End Date' },
-  { name: 'productClass', label: 'Product Class' },
+	{ name: 'version', label: 'Version' },
+	{ name: 'startDate', label: 'Start Date' },
+	{ name: 'endDate', label: 'End Date' },
+	{ name: 'productClass', label: 'Product Class' },
 ];
 
 const versionData = [
-  { version: 2, startDate: '6/5/2018 03:55:34 PM', endDate: '', productClass: 'CNDC PS' },
-  { version: 1, startDate: '4/5/2018 04:22:18 PM', endDate: '6/5/2018 03:55:34 PM', productClass: 'CNDC PS' },
-  // Add more rows here
+	{ version: 2, startDate: '6/5/2018 03:55:34 PM', endDate: '', productClass: 'CNDC PS' },
+	{ version: 1, startDate: '4/5/2018 04:22:18 PM', endDate: '6/5/2018 03:55:34 PM', productClass: 'CNDC PS' },
+	// Add more rows here
 ];
 
 const ProductNew: React.FC = () => {
@@ -99,84 +99,82 @@ const ProductNew: React.FC = () => {
 								responsive: 'standard',
 								}}
 							/>
-					</Box>
-				<AppBar position="static" style={{ marginTop: '20px' }}>
-					<Tabs
-					value={value}
-					onChange={handleChange}
-					variant={isMobile ? 'scrollable' : 'standard'}
-					scrollButtons={isMobile}
-					>
-					<Tab label="Products" />
-					<Tab label="Versions" />
-					<Tab label="User Defined Attributes" />
-					<Tab label="More Info" />
-					<Tab label="Eligibility and Compatibility Rules" />
-					<Tab label="Pricing" />
-					<Tab label="Recommendations" />
-					<Tab label="Collateral" />
-					<Tab label="Translations" />
-					<Tab label="Service Information" />
-					<Tab label="Product Validation" />
-					<Tab label="Bundle Product" />
-					<Tab label="Category" />
-					<Tab label="More Info" />
-					</Tabs>
-				</AppBar>
-				<Container>
-					<TabPanel value={value} index={0}>
-						<Typography variant="h6">Product Details</Typography>
-					</TabPanel>
-					<TabPanel value={value} index={1}>
-						<MUIDataTable
-							title={"Version History"}
-							data={versionData}
-							columns={versionColumns}
-							options={{
-								selectableRows: 'none',
-								rowsPerPage: 5,
-								rowsPerPageOptions: [5, 10, 15],
-								responsive: 'vertical',
-							}}
-						/>
-					</TabPanel>
-					<TabPanel value={value} index={2}>
-						<Typography variant="h6">User Defined Attributes</Typography>
-					</TabPanel>
-					<TabPanel value={value} index={3}>
-						<Typography variant="h6">More Info</Typography>
-					</TabPanel>
-					<TabPanel value={value} index={4}>
-						<Typography variant="h6">Eligibility and Compatibility Rules</Typography>
-					</TabPanel>
-					<TabPanel value={value} index={5}>
-						<Typography variant="h6">Pricing</Typography>
-					</TabPanel>
-					<TabPanel value={value} index={6}>
-						<Typography variant="h6">Recommendations</Typography>
-					</TabPanel>
-						<TabPanel value={value} index={7}>
-					<Typography variant="h6">Collateral</Typography>
-					</TabPanel>
-						<TabPanel value={value} index={8}>
-					<Typography variant="h6">Translations</Typography>
+						<Tabs
+						value={value}
+						onChange={handleChange}
+						variant={isMobile ? 'scrollable' : 'standard'}
+						scrollButtons={isMobile}
+						>
+							<Tab label="Products" />
+							<Tab label="Versions" />
+							<Tab label="User Defined Attributes" />
+							<Tab label="More Info" />
+							<Tab label="Eligibility and Compatibility Rules" />
+							<Tab label="Pricing" />
+							<Tab label="Recommendations" />
+							<Tab label="Collateral" />
+							<Tab label="Translations" />
+							<Tab label="Service Information" />
+							<Tab label="Product Validation" />
+							<Tab label="Bundle Product" />
+							<Tab label="Category" />
+							<Tab label="More Info" />
+						</Tabs>
+						<TabPanel value={value} index={0}>
+							<Typography variant="h6">Product Details</Typography>
 						</TabPanel>
-					<TabPanel value={value} index={9}>
-						<Typography variant="h6">Service Information</Typography>
-					</TabPanel>
-					<TabPanel value={value} index={10}>
-						<Typography variant="h6">Product Validation</Typography>
-					</TabPanel>
-					<TabPanel value={value} index={11}>
-						<Typography variant="h6">Bundle Product</Typography>
-					</TabPanel>
-					<TabPanel value={value} index={12}>
-						<Typography variant="h6">Category</Typography>
-					</TabPanel>
-					<TabPanel value={value} index={13}>
-						<Typography variant="h6">More Info</Typography>
-					</TabPanel>
-				</Container>
+						<TabPanel value={value} index={1}>
+							<Box sx={{ width:'100%' }}>
+								<MUIDataTable
+									title={"Version History"}
+									data={versionData}
+									columns={versionColumns}
+									options={{
+										selectableRows: 'none',
+										rowsPerPage: 5,
+										rowsPerPageOptions: [5, 10, 15],
+										responsive: 'vertical',
+									}}
+								/>
+							</Box>
+						</TabPanel>
+						<TabPanel value={value} index={2}>
+							<Typography variant="h6">User Defined Attributes</Typography>
+						</TabPanel>
+						<TabPanel value={value} index={3}>
+							<Typography variant="h6">More Info</Typography>
+						</TabPanel>
+						<TabPanel value={value} index={4}>
+							<Typography variant="h6">Eligibility and Compatibility Rules</Typography>
+						</TabPanel>
+						<TabPanel value={value} index={5}>
+							<Typography variant="h6">Pricing</Typography>
+						</TabPanel>
+						<TabPanel value={value} index={6}>
+							<Typography variant="h6">Recommendations</Typography>
+						</TabPanel>
+							<TabPanel value={value} index={7}>
+						<Typography variant="h6">Collateral</Typography>
+						</TabPanel>
+							<TabPanel value={value} index={8}>
+						<Typography variant="h6">Translations</Typography>
+							</TabPanel>
+						<TabPanel value={value} index={9}>
+							<Typography variant="h6">Service Information</Typography>
+						</TabPanel>
+						<TabPanel value={value} index={10}>
+							<Typography variant="h6">Product Validation</Typography>
+						</TabPanel>
+						<TabPanel value={value} index={11}>
+							<Typography variant="h6">Bundle Product</Typography>
+						</TabPanel>
+						<TabPanel value={value} index={12}>
+							<Typography variant="h6">Category</Typography>
+						</TabPanel>
+						<TabPanel value={value} index={13}>
+							<Typography variant="h6">More Info</Typography>
+						</TabPanel>
+					</Box>
 				</Box>
 			</Box>
 		</Box>
