90d7b78ad908007f0746164f679227141c3b596a
Fatur Muhammad <fatur_muhammad@bk.ru>
2024-12-19 12:29:36 +0700

    add obfuscator

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from "url"
+import obfuscator from 'rollup-plugin-obfuscator';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    obfuscator({
+      options: {
+				// Your javascript-obfuscator options here
+				// See what's allowed: https://github.com/javascript-obfuscator/javascript-obfuscator
+        transformObjectKeys: true,
+        unicodeEscapeSequence: true,
+        numbersToExpressions: true,
+        shuffleStringArray: true,
+        splitStrings: true,
+        stringArrayThreshold: 1,
+        identifierNamesGenerator: 'hexadecimal'
+			},
+    })
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
